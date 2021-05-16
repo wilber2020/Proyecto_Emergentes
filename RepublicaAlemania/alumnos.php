4d95eba80531ed63d9230361700e151061ed82fa
wilber2020 <61986449+wilber2020@users.noreply.github.com>
2021-05-15 21:38:31 -0600

    Add files via upload

---
+++ RepublicaAlemania/alumnos.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>República de Alemania</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sidebars/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="sidebars.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php  
+    include("disenos/imgSvg.php");
+  ?>
+<main>
+  <?php  
+    include("disenos/navbar.php");
+  ?>
+<div class="row" style="padding-left: 5%;">
+  <h3 style="padding-top: 3%;">Alumnos</h3>
+  
+  <div class="col-md-12" style="width: 1100px; position: relative;top: -300px;">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Nombres</th>
+          <th scope="col">Apellidos</th>
+          <th scope="col">Fecha de Nacimiento</th>
+          <th scope="col">Sexo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          require_once("pruebaC.php");  
+          /*isset($_GET["grado"]);
+          $grad= $_GET["grado"];
+          $nuevo = array('grado.grado'=>$grad);*/
+          $datos = $alumno->find();
+
+          foreach($datos as $dato){ ?>
+            <tr>
+            <th><?php echo $dato["_id"]; ?></th>
+                  <th><?php echo $dato["nombre"]; ?></th>
+                  <td><?php echo $dato['apellido']; ?></td>
+                  <td><?php echo $dato['fechaNacimiento']; ?></td>
+                  <td><?php echo $dato['sexo']; ?></td>
+                  <td><a href="modificarA.php?idA=<?php echo $dato["_id"];?>"><button type='button' class="btn btn-primary">Modificar</button></a></td>
+                  <td><a href="EliminarA.php?id=<?php echo $dato["_id"];?>"><button type='button' class="btn btn-danger">Eliminar</button></a></td>
+                </tr>
+            <?php
+          }
+        ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+</main>
+
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      <script src="sidebars.js"></script>
+  </body>
+</html>
---
+++ RepublicaAlemania/disenos/navbar.php
@@ -0,0 +1,51 @@
+<div class="d-flex flex-column p-3 text-white bg-dark" style="width: 280px;">
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <strong>Nombre Usuario</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="#">Salir</a></li>
+      </ul>
+    </div>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="index.php" class="nav-link active" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+          Inicio
+        </a>
+      </li>
+      <li>
+        <a href="usuario.php" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+          Usuarios
+        </a>
+      </li>
+      <li>
+        <a href="alumnos.php" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+          Alumnos
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+          Notas
+        </a>
+      </li>
+      <li>
+        <a href="matricula.php" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+          Matrícula
+        </a>
+      </li>
+      <li>
+        <a href="matricula.php" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+          Grados
+        </a>
+      </li>
+    </ul>
+    <div style="padding-top: 190%;"></div>
+    <hr>
+  </div>
